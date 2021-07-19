7c3a9fe03097da116e57c73eda69cdb138be77cb
jinwu09 <bjaj09yahoo.com@gmail.com>
2021-07-20 00:18:55 +0800

    register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'main',
     pathMatch: 'full'
+  },  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
+
 ];
 
 @NgModule({
